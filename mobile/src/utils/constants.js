e0a81dd910368275930926801b2486111f518578
Alec <alecperkey@gmail.com>
2017-10-05 16:53:02 -0700

    actualize genymotion fix

--- mobile/src/utils/constants.js
+++ mobile/src/utils/constants.js
@@ -8,7 +8,7 @@ export const colors = {
 export const fakeAvatar = 'https://pbs.twimg.com/profile_images/835144746217664515/oxBgzjRA_bigger.jpg';
 
 
-export const REACT_NATIVE_PACKAGER_HOSTNAME = 'localhost';
+// export const REACT_NATIVE_PACKAGER_HOSTNAME = 'localhost';
 
 // fix for windows android for using exp start --lan
 // - using tunnel  on android with debugging is unusable
@@ -17,4 +17,4 @@ export const REACT_NATIVE_PACKAGER_HOSTNAME = 'localhost';
 // - more info:
 // https://docs.expo.io/versions/latest/guides/debugging.html#to-ensure-the-best-debugging-experience-first-change-your-host-type-in-xde-to-lan-or-localhost-if-you-use-tunnel-with-debugging-enabled-you-are-likely-to-experience-so-much-latency-that-your-app-is-unusable-while-here-also-ensure-that-development-mode-is-checked
 // https://github.com/react-community/create-react-native-app/issues/81
-// export const REACT_NATIVE_PACKAGER_HOSTNAME = '192.168.1.X';
+export const REACT_NATIVE_PACKAGER_HOSTNAME = '192.168.1.15';
